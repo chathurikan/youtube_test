d35d9267e78141a08cf21a90bcbf6f8ad284f5d3
chathubi <chathurikananayakkarasecond@gmail.com>
2024-05-31 17:02:17 +0530

    second_modification

---
+++ first_test_repository.py
@@ -0,0 +1,10 @@
+first_name= 'Chathurika'
+last_name='Nanayakkara'
+print('my firse name is {}  my last name is {}, so my full name is {} {}'.format(first_name,last_name,first_name,last_name))
+
+
+address = "Boralesgamuwa"
+print (len(address))
+print(address[0])
+print(address[5])
+print(address[-2])
